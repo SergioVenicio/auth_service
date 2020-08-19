e9afe313296868ad13c6da51fef565915243bc79
sergio <sergio.andrade@sensedata.com.br>
2020-08-19 00:52:11 -0300

    Remove unnecessary method

--- models/User.py
+++ models/User.py
@@ -34,10 +34,5 @@ class User(db.Model):
         if user and user.check_password(password):
             return user
 
-    @staticmethod
-    def identity(payload):
-        user_id = payload['identity']
-        return User.query.get(user_id)
-
     def __str__(self):
         return f'User: {self.username}'
